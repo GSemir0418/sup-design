e4b6b1ed4fe174ea986559099ef4336fe55c9661
gsemir0418 <845217811@qq.com>
2022-06-13 17:21:24 +0800

    chore: 构建lib的配置（有bug）

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
+const path = require('path')
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.ts'),
+      name: 'MyLib',
+      formats: ['es'],
+      fileName: format => `my-lib.${format}.js`,
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['react', 'react-dom'],
+    },
+  },
 })
