16c196049f289d0f8237c71ce1ac6b5a499947d8
gsemir0418 <845217811@qq.com>
2022-06-20 11:00:43 +0800

    chore: 配置svg雪碧图插件

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+// 在 src/main.ts 内引入svg雪碧图插件的注册脚本
+import 'virtual:svg-icons-register'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,31 @@
 import { defineConfig } from 'vite'
 const path = require('path')
 import react from '@vitejs/plugin-react'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createSvgIconsPlugin({
+      // 指定需要缓存的图标文件夹
+      iconDirs: [path.resolve(process.cwd(), 'src/components/icon/icons')],
+      // 指定symbolId格式
+      symbolId: 'icon-[name]',
+
+      /**
+       * 自定义插入位置
+       * @default: body-last
+       */
+      inject: 'body-first',
+
+      /**
+       * custom dom id
+       * @default: __svg__icons__dom__
+       */
+      customDomId: '__svg__icons__dom__',
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/components/index.ts'),
